c0bf6a066fc31641e74c52dda46460a13cf9ada4
Odin Guy <odinscode@gmail.com>
2018-10-07 18:11:31 +0300

    Task 1: .Net Core
    Hello world on .Net Core platform

---
+++ Module 1/Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayGrettings(args[0]);
+        }
+
+        static void SayGrettings(string personName)
+        {
+            System.Console.WriteLine($"Hello, {personName}!");
+        }
+    }
+}
