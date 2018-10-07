4dccf9bc3a0786c112cee97f103f0e5f91fa1cbb
Odin Guy <odinscode@gmail.com>
2018-10-07 18:54:22 +0300

    Task 1: .Net Standard
    Hello world WFA .Net Standard

---
+++ Module 1/Standard/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GreetingsSolution
+{
+    public partial class GreetingForm : Form
+    {
+        public GreetingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GreetingButton_Click(object sender, EventArgs e)
+        {
+            var userName = this.EnterNameTextbox.Text;
+
+            if (isNameEntered(userName))
+            {
+                MessageBox.Show($"Hello, {userName}!");
+            }
+            else
+            {
+                MessageBox.Show($"Please enter your name!");
+            }
+        }
+
+        private static bool isNameEntered(string userName)
+        {
+            return !string.IsNullOrEmpty(userName);
+        }
+    }
+}
