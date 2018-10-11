3067886b015cd8c2d6bf81ed9a0be516bec1f205
Odin Guy <odinscode@gmail.com>
2018-10-11 18:14:53 +0300

    Task 3: comments fixed
    Fixed comments

--- Module 1/Task 3/GreetingService.cs
+++ Module 1/Task 3/GreetingService.cs
@@ -4,18 +4,26 @@ namespace GreetingsLibrary
 {
     public static class GreetingService
     {
+        private const string ANONYMOUS = "Anonymous";
+
         public static string GreetPerson(string personName)
         {
-            if (String.IsNullOrEmpty(personName))
+            var user = personName;
+            if (IsUserInputInvalid(personName))
             {
-                return $"{GetFormattedCurrentTime()} Hello, anonymous!";
+                user = ANONYMOUS;
             }
-            return $"{GetFormattedCurrentTime()} Hello, {personName}!";
+            return $"{GetFormattedCurrentTime()} Hello, {user}!";
         }
 
         public static string GetFormattedCurrentTime()
         {
             return DateTime.Now.ToString("h:mm:ss tt");
+        }
+
+        private static bool IsUserInputInvalid(string userInput)
+        {
+            return string.IsNullOrEmpty(userInput) || string.IsNullOrWhiteSpace(userInput);
         }
     }
 }
