6fb218ea92a608b68073b5ac8d35cb80c75ac60c
Odin Guy <odinscode@gmail.com>
2018-10-31 10:14:35 +0300

    Module 2 Task 2: refactored first test

--- Module 2/FileSystemVisitorAppTests/Services/FileSearcherTests.cs
+++ Module 2/FileSystemVisitorAppTests/Services/FileSearcherTests.cs
@@ -2,6 +2,7 @@
 using FileSystemVisitorApp.Models;
 using FileSystemVisitorApp.Services;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -10,46 +11,34 @@ namespace FileSystemVisitorAppTests.Services
 {
     public class FileSearcherTests
     {
-        private static readonly string RootDirectoryPath = 
+        private static readonly string RootDirectoryPath =
             @"X:\Temp";
-        private static readonly string SecondIterationDirectoryPath = 
+        private static readonly string SecondIterationDirectoryPath =
             string.Concat(RootDirectoryPath, @"\", "Directory1");
 
-        private static readonly string FirstIterationFileName = 
+        private static readonly string FirstIterationFileName =
             string.Concat(RootDirectoryPath, @"\", "Test1.txt");
-        private static readonly string SecondIterationFileName = 
+        private static readonly string SecondIterationFileName =
             string.Concat(SecondIterationDirectoryPath, @"\", "Test2.txt");
 
         [Fact]
         public void GetItemsRecursively_WhenMaskNone_ShouldReturnAllFilesAndDirectories()
         {
             // Arrange
+            var filesRootMock = GetMockedFiles(new string[] { FirstIterationFileName });
+            var filesSecondIterationMock = GetMockedFiles(new string[] { SecondIterationFileName });
+
             var directoriesFirstIterationMock = new Mock<List<Mock<CustomDirectoryInfo>>>();
-            var directoriesSecondIterationMock = new Mock<CustomDirectoryInfo>(SecondIterationDirectoryPath);
-
-            var filesSecondIterationMock = new Mock<List<Mock<CustomFileInfo>>>(); 
-            var fileSecondIterationMock = new Mock<CustomFileInfo>(SecondIterationFileName);
-            filesSecondIterationMock.Object.Add(fileSecondIterationMock);
-
-            directoriesSecondIterationMock.Setup(_ => _.GetFiles())
-                .Returns(filesSecondIterationMock.Object.Select(x => x.Object));
+            var directoriesSecondIterationMock = GetMockedDirectory(SecondIterationDirectoryPath, filesSecondIterationMock);
             directoriesFirstIterationMock.Object.Add(directoriesSecondIterationMock);
 
-            var filesRootMock = new Mock<List<Mock<CustomFileInfo>>>();
-            var fileFirstIterationMock = new Mock<CustomFileInfo>(FirstIterationFileName);
-            filesRootMock.Object.Add(fileFirstIterationMock);
+            var directoryRootMock = GetMockedDirectory(RootDirectoryPath, filesRootMock, directoriesFirstIterationMock);
 
-            var directoryRootMock = new Mock<CustomDirectoryInfo>(RootDirectoryPath);
-            directoryRootMock.Setup(_ => _.GetDirectories())
-                .Returns(directoriesFirstIterationMock.Object.Select(x => x.Object));
-            directoryRootMock.Setup(_ => _.GetFiles())
-                .Returns(filesRootMock.Object.Select(x => x.Object));
+            var factoryInstances = new Dictionary<string, Mock<CustomDirectoryInfo>>();
+            factoryInstances.Add(RootDirectoryPath, directoryRootMock);
+            factoryInstances.Add(SecondIterationDirectoryPath, directoriesSecondIterationMock);
 
-            var factoryMock = new Mock<ICustomDirectoryInfoFactory>();
-            factoryMock.Setup(_ => _.CreateInstance(RootDirectoryPath))
-                .Returns(directoryRootMock.Object);
-            factoryMock.Setup(_ => _.CreateInstance(SecondIterationDirectoryPath))
-                .Returns(directoriesSecondIterationMock.Object);
+            var factoryMock = GetMockedFactories(factoryInstances);
 
             var fileSearcher = new FileSearcher(factoryMock.Object, RootDirectoryPath);
             var filterMask = FilterMask.None;
@@ -64,6 +53,50 @@ namespace FileSystemVisitorAppTests.Services
 
             Assert.Equal(2, filesCount);
             Assert.Equal(2, directoriesCount);
+        }
+
+        private Mock<ICustomDirectoryInfoFactory> GetMockedFactories(Dictionary<string, Mock<CustomDirectoryInfo>> instances)
+        {
+            var factoryMock = new Mock<ICustomDirectoryInfoFactory>();
+            foreach (var instanceName in instances.Keys)
+            {
+                factoryMock.Setup(_ => _.CreateInstance(instanceName))
+                    .Returns(instances[instanceName].Object);
+            }
+            return factoryMock;
+        }
+
+        private Mock<List<Mock<CustomFileInfo>>> GetMockedFiles(string[] fileNames)
+        {
+            var filesRootMock = new Mock<List<Mock<CustomFileInfo>>>();
+            foreach (var fileName in fileNames)
+            {
+                var fileMock = new Mock<CustomFileInfo>(fileName);
+                filesRootMock.Object.Add(fileMock);
+            }
+            return filesRootMock;
+        }
+
+        private Mock<CustomDirectoryInfo> GetMockedDirectory(string directoryPath)
+        {
+            var directoryMock = new Mock<CustomDirectoryInfo>(directoryPath);
+            return directoryMock;
+        }
+
+        private Mock<CustomDirectoryInfo> GetMockedDirectory(string directoryPath, Mock<List<Mock<CustomFileInfo>>> internalFiles = null)
+        {
+            var directoryMock = GetMockedDirectory(directoryPath);
+            directoryMock.Setup(_ => _.GetFiles())
+                .Returns(internalFiles.Object.Select(x => x.Object));
+            return directoryMock;
+        }
+
+        private Mock<CustomDirectoryInfo> GetMockedDirectory(string directoryPath, Mock<List<Mock<CustomFileInfo>>> internalFiles, Mock<List<Mock<CustomDirectoryInfo>>> internalDirectories)
+        {
+            var directoryMock = GetMockedDirectory(directoryPath, internalFiles);
+            directoryMock.Setup(_ => _.GetDirectories())
+                .Returns(internalDirectories.Object.Select(x => x.Object));
+            return directoryMock;
         }
     }
 }
