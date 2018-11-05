055e8213f48538daf262e8592a2637b619243457
Odin Guy <odinscode@gmail.com>
2018-11-05 19:24:23 +0300

    m4t1 - first 4 methods

--- Module 4/Task/LinqSamples.cs
+++ Module 4/Task/LinqSamples.cs
@@ -1,65 +1,101 @@
-﻿// Copyright © Microsoft Corporation.  All Rights Reserved.
-// This code released under the terms of the 
-// Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.)
-//
-//Copyright (C) Microsoft Corporation.  All rights reserved.
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System;
 using System.Linq;
-using System.Windows.Forms;
-using System.Xml.Linq;
 using SampleSupport;
 using Task.Data;
 
-// Version Mad01
-
 namespace SampleQueries
 {
-	[Title("LINQ Module")]
-	[Prefix("Linq")]
-	public class LinqSamples : SampleHarness
-	{
+    [Title("LINQ Module")]
+    [Prefix("Linq")]
+    public class LinqSamples : SampleHarness
+    {
+        private DataSource dataSource = new DataSource();
 
-		private DataSource dataSource = new DataSource();
+        [Category("Restriction Operators")]
+        [Title("Where - Task 1")]
+        [Description("Amount of customers whose Total turnover is greater than X")]
+        public void Linq1()
+        {
+            int totalTurnover = 0;
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
-		public void Linq1()
-		{
-			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var customers = dataSource.Customers
+                .ToDictionary(
+                    k => k,
+                    v => v.Orders.Sum(o => o.Total))
+                .Where(f => f.Value > totalTurnover);
 
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
+            while (customers.Count() > 5)
+            {
+                Console.WriteLine($"Amount of customers whose Total turnover is greater than {totalTurnover} = {customers.Count()}");
+                foreach (var item in customers)
+                {
+                    Console.WriteLine($"{item.Key.CustomerID} has total turnover equal to {item.Value}");
+                }
+                totalTurnover += 4000;
+                Console.WriteLine();
+            }
+        }
 
-			Console.WriteLine("Numbers < 5:");
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
-		}
+        [Category("Restriction Operators")]
+        [Title("Where - Task 2")]
+        [Description("Customer's list of suppliers from same location")]
+        public void Linq2()
+        {
+            var customers = dataSource.Customers
+                .Select(customer => new { customer, suppliers = dataSource.Suppliers.Where(supplier => supplier.Country == customer.Country && supplier.City == customer.City) });
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
+            foreach (var item in customers)
+            {
+                Console.WriteLine($"Customer's ID is - {item.customer.CustomerID}, customer's country is - {item.customer.Country}, customer's city is {item.customer.City}");
 
-		public void Linq2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+                if (item.suppliers.Count() > 0)
+                {
+                    Console.WriteLine($"Supplier's name | Supplier's country | Supplier's city");
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
+                    foreach (var supplier in item.suppliers)
+                    {
+                        Console.WriteLine($"{supplier.SupplierName} | {supplier.Country} | {supplier.City}");
+                    }
 
-	}
+                }
+                Console.WriteLine();
+            }
+        }
+
+        [Category("Heh")]
+        [Title("Any - Task 3")]
+        [Description("Customers who had at least one order which cost was more than X")]
+        public void Linq3()
+        {
+            int cost = 5000;
+
+            var result = dataSource.Customers
+                .Select(c => new { customer = c, order = c.Orders.FirstOrDefault(o => o.Total > cost) });
+
+            foreach (var item in result)
+            {
+                if (item.order != null)
+                {
+                    Console.WriteLine($"Customer's id: {item.customer.CustomerID}, first order with cost > {cost}: {item.order.OrderID}");
+                }
+            }
+        }
+
+        [Category("Heh")]
+        [Title("Any - Task 4")]
+        [Description("Customers who had at least one order which cost was more than X")]
+        public void Linq4()
+        {
+            var result = dataSource.Customers
+                .Select(c => new { customer = c, firstOrderDate = c?.Orders?.Min(o => o.OrderDate) });
+
+            foreach (var item in result)
+            {
+                if (item.firstOrderDate != null)
+                {
+                    Console.WriteLine($"Customer's id: {item.customer.CustomerID}, first order date {item.firstOrderDate}");
+                }
+            }
+        }
+    }
 }
--- Module 4/Task/Program.cs
+++ Module 4/Task/Program.cs
@@ -1,20 +1,11 @@
-﻿// Copyright © Microsoft Corporation.  All Rights Reserved.
-// This code released under the terms of the 
-// Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.)
-//
-//Copyright (C) Microsoft Corporation.  All rights reserved.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using SampleSupport;
-using SampleQueries;
-using System.IO;
 
-// See the ReadMe.html for additional information
 namespace SampleQueries
 {
-	static class Program
+    static class Program
 	{
 		/// <summary>
 		/// The main entry point for the application.
@@ -24,16 +15,16 @@ namespace SampleQueries
 		{
 			List<SampleHarness> harnesses = new List<SampleHarness>();
 
-			
-			LinqSamples linqHarness = new LinqSamples();
-			harnesses.Add(linqHarness);
-						
-			Application.EnableVisualStyles();
+            var linqHarness = new LinqSamples();
+
+            harnesses.Add(linqHarness);
+
+            Application.EnableVisualStyles();
 				
-			using (SampleForm form = new SampleForm("HomeWork - Mihail Romanov", harnesses))
+			using (SampleForm form = new SampleForm("HomeWork - Vyacheslav Odinokov", harnesses))
 			{
 				form.ShowDialog();
 			}
 		}
-	}
+    }
 }
